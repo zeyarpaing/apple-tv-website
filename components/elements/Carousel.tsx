d121d9a92369dcce1b7ec0182cd37dd128014ccf
Zeyar Paing <zeyarpaing@ucsy.edu.mm>
2022-06-15 13:31:21 +0630

    style: adjust with responsive

--- components/elements/Carousel.tsx
+++ components/elements/Carousel.tsx
@@ -32,6 +32,23 @@ const Carousel = ({ scrollY }: { scrollY: number }) => {
     'essex-serpent',
   ]);
   const [xShift, setXShift] = useState(0);
+  const [minWidth, setMinWidth] = useState(0);
+  const [minHeight, setMinHeight] = useState(0);
+
+  function applyResponsiveWidth() {
+    let width = window.innerWidth;
+    if (width > 1068) {
+      setMinWidth(900);
+      setMinHeight(506);
+    } else if (width > 833) {
+      setMinWidth(555);
+      setMinHeight(312);
+    } else {
+      //mobile
+      setMinWidth(278);
+      setMinHeight(494);
+    }
+  }
 
   useEffect(() => {
     // setInterval(() => {
@@ -40,8 +57,11 @@ const Carousel = ({ scrollY }: { scrollY: number }) => {
     //   let newMovie = [...movies.slice(1, movies.length), movies[0]];
     //   setMovies(newMovie);
     // }, 700);
+    applyResponsiveWidth();
+    window.addEventListener('resize', (e) => {
+      applyResponsiveWidth();
+    });
   }, []);
-  console.log('scrollY : ', scrollY);
   return (
     <div className={styles.carouselWrapper}>
       <div>
@@ -56,9 +76,13 @@ const Carousel = ({ scrollY }: { scrollY: number }) => {
               backgroundImage: `url('/movies/${
                 movies[Math.floor(movies.length / 2)]
               }.jpg')`,
-              width: `max(900px, calc(110rem - ${scrollY - 1500 + 'px'} ))`,
+              width: `max(${minWidth}px, calc(110rem - ${
+                scrollY - 1500 + 'px'
+              } ))`,
               // bottom: `max(0px, -${scrollY - 1000}px)`,
-              height: `max(506px, calc(110rem - ${scrollY - 1000 + 'px'} ))`,
+              height: `max(${minHeight}px, calc(110rem - ${
+                scrollY - 1000 + 'px'
+              } ))`,
               // top: scrollY - 1800 + 'px',
             }}
           >
